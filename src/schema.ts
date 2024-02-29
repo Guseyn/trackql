8a249f69029a1d7ba068b6135b0a4532ac6879c3
guseyn <guseyn@guseyn.com>
2024-02-29 20:42:43 +0400

    clean up

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,8 @@
 import { readFileSync } from 'fs';
 import { buildSchema } from 'graphql';
-import { loadSchemaSync } from '@graphql-tools/load';
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 
 const schemaFilePath = './src/graphql/schema.graphql';
 
 const schemaString = readFileSync(schemaFilePath, 'utf8');
-
-const loadedSchema = loadSchemaSync(schemaFilePath, {
-  loaders: [new GraphQLFileLoader()],
-});
 
 export const SCHEMA = buildSchema(schemaString)
